d388263667f757c22d52c79aed52d0c5fb91f37e
Umut Barış Öztunç <umutoztunc@gmail.com>
2022-01-14 15:57:03 +0300

    Create prime.h and introduce is_prime function
    This commit creates number_theory/prime.h header file and implements
    is_prime function for integer numbers <= 2^16.

---
+++ number_theory/prime.h
@@ -0,0 +1,32 @@
+#ifndef NUMBER_THEORY_PRIME_H_
+#define NUMBER_THEORY_PRIME_H_
+
+#include <limits>
+#include <type_traits>
+
+// This file contains functions related to prime numbers.
+
+namespace number_theory {
+
+// Tests whether |number| is prime or not.
+//
+// This overload is only available for numbers smaller than 2^16 since it
+// performs poorly for large numbers.
+template <class T,
+          std::enable_if_t<std::numeric_limits<T>::is_integer &&
+                               std::numeric_limits<T>::digits <= 16,
+                           bool> = true>
+constexpr bool is_prime(T number) {
+  // There are no prime numbers smaller than 2.
+  if (number < 2)
+    return false;
+  for (int i = 2; i * i <= number; ++i) {
+    if (number % i == 0)
+      return false;
+  }
+  return true;
+}
+
+}  // namespace number_theory
+
+#endif  // NUMBER_THEORY_PRIME_H_
