bb1c53ad20511d53a11a92ca77d614a5d36ac723
Jiashuo Liang <liangjs@pku.edu.cn>
2022-01-19 21:12:53 +0800

    Rename exgcd test

--- number_theory/numeric_unittest.cpp
+++ number_theory/numeric_unittest.cpp
@@ -8,7 +8,8 @@
 
 #include "number_theory/numeric.h"
 
-using namespace tql::number_theory;
+namespace tql {
+namespace number_theory {
 
 template <class T>
 void test_exgcd(T a, T b) {
@@ -21,17 +22,17 @@ void test_exgcd(T a, T b) {
   ASSERT_EQ(x * a + y * b, gcd(a, b));
 }
 
-TEST(NumericTest, Exgcd) {
-  for (int a = -100; a <= 100; ++a)
-    for (int b = -100; b <= 100; ++b) {
+TEST(NumericTest, ExGCD) {
+  for (int a = -10; a <= 100; ++a)
+    for (int b = -100; b <= 10; ++b) {
       test_exgcd<int8_t>(a, b);
       test_exgcd<int16_t>(a, b);
       test_exgcd<int32_t>(a, b);
       test_exgcd<int64_t>(a, b);
     }
   // test large numbers
-  std::random_device rand_device;
-  std::default_random_engine rand_engine(rand_device());
+  std::default_random_engine rand_engine(
+      testing::UnitTest::GetInstance()->random_seed());
   std::uniform_int_distribution<int64_t> rand_gen(
       std::numeric_limits<int64_t>::min(), std::numeric_limits<int64_t>::max());
   for (int i = 0; i < 1000; ++i) {
@@ -40,3 +41,6 @@ TEST(NumericTest, Exgcd) {
     test_exgcd(a, b);
   }
 }
+
+}  // namespace number_theory
+}  // namespace tql
