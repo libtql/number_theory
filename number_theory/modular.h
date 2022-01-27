3d084e72a693cceff7d0a39ae5a652dec77ef302
liangjs <liangjs@pku.edu.cn>
2022-01-27 16:32:11 +0800

    Implement basic modular arithmetic
    This starts a series of the modular arithmetic implementation.

---
+++ number_theory/modular.h
@@ -0,0 +1,109 @@
+#ifndef NUMBER_THEORY_MODULAR_H_
+#define NUMBER_THEORY_MODULAR_H_
+
+#include <stddef.h>
+
+#include <bit>
+#include <limits>
+
+#include "number_theory/utility.h"
+
+// Modular arithmetic
+
+namespace tql {
+namespace number_theory {
+
+namespace {
+
+// Returns the equivalent element of |x| in the ring of integers modulo
+// |modulus|. The result |y| should statisfy 0 <= y < modulus, and
+// y = k*modulus + x for some integer k.
+template <class T,
+          std::enable_if_t<std::numeric_limits<T>::is_integer, bool> = true>
+constexpr T normalize(T x, T modulus) {
+  T y = std::move(x);
+  if (y < 0 || y >= modulus) {
+    y %= modulus;
+    if (y < 0)
+      y += modulus;
+  }
+  return y;
+}
+
+}  // namespace
+
+template <class T, T mod>
+class Modular {
+  static_assert(std::numeric_limits<T>::is_integer && mod > 0,
+                "Modular requires modulus to be a positive integer.");
+
+ public:
+  using type = T;
+  static constexpr T modulus = mod;
+
+  // This is an implicit constructor. We implicitly upgrade from T to Modular
+  // to make it easier to use.
+  Modular(T value = 0) { set(std::move(value)); }
+
+  Modular(const Modular &other) = default;
+  Modular(Modular &&other) = default;
+  Modular &operator=(const Modular &other) = default;
+  Modular &operator=(Modular &&other) = default;
+
+  // This is an implicit conversion, because we want a seamless conversion to T.
+  operator T() const { return get(); }
+
+  const T &get() const { return value_; }
+
+  void set(T value) { value_ = normalize(std::move(value), modulus); }
+
+  Modular add(const Modular &rhs) const {
+    check_addition_overflow();
+    T new_value = value_ + rhs.value_;
+    if (new_value >= modulus)
+      new_value -= modulus;
+    return Modular(new_value);
+  }
+
+  Modular negate() const {
+    if (value_ == 0)
+      return *this;
+    return Modular(modulus - value_);
+  }
+
+  Modular substract(const Modular &rhs) const { return add(rhs.negate()); }
+
+  Modular multiply(const Modular &rhs) const {
+    check_multiplication_overflow();
+    return Modular(value_ * rhs.value_);
+  }
+
+  bool equal(const Modular &rhs) const { return value_ == rhs.value_; }
+
+ protected:
+  T value_;
+
+  static constexpr size_t type_width = std::numeric_limits<T>::digits;
+  static constexpr size_t modulus_width =
+      std::bit_width(static_cast<std::make_unsigned_t<T>>(modulus));
+
+  void check_addition_overflow() const {
+    static_assert(
+        modulus_width + 1 <= type_width,
+        "Modular addition may overflow. Please use larger integer types.");
+  }
+
+  void check_multiplication_overflow() const {
+    static_assert(modulus_width * 2 <= type_width,
+                  "Modular multiplication may overflow. "
+                  "Please use larger integer types.");
+  }
+};
+
+}  // namespace number_theory
+
+using number_theory::Modular;
+
+}  // namespace tql
+
+#endif  // NUMBER_THEORY_MODULAR_H_
---
+++ number_theory/modular_unittest.cpp
@@ -0,0 +1,77 @@
+#include <stdint.h>
+
+#include <limits>
+#include <type_traits>
+
+#include <gtest/gtest.h>
+
+#include "number_theory/modular.h"
+
+namespace tql {
+namespace number_theory {
+
+template <class T>
+void test_modular_basic() {
+  using Mod10 = Modular<T, T(10)>;
+  static_assert(std::is_same<typename Mod10::type, T>::value);
+  static_assert(Mod10::modulus == T(10));
+
+  // test constructors
+  Mod10 a = T(123);
+  Mod10 b = a;
+  Mod10 c;
+  EXPECT_EQ(a.get(), T(3));
+  EXPECT_EQ(b.get(), T(3));
+  EXPECT_EQ(c.get(), T(0));
+
+  // test assignments
+  if (std::numeric_limits<T>::is_signed) {
+    c = T(-1);
+    a.set(T(-4));
+  } else {
+    c = T(9);
+    a.set(T(6));
+  }
+  EXPECT_EQ(c.get(), T(9));
+  EXPECT_EQ(a.get(), T(6));
+  a = std::move(c);
+  EXPECT_EQ(a.get(), T(9));
+
+  // test conversions
+  a = T(3);
+  EXPECT_EQ(static_cast<T>(a), T(3));
+  EXPECT_EQ(static_cast<Mod10>(T(15)).get(), T(5));
+  // implicit conversions
+  EXPECT_EQ(a, 3);
+  EXPECT_EQ(Mod10(15), 5);
+
+  // test arithmetics
+  a = T(6), b = T(4);
+  c = a.add(b);
+  EXPECT_EQ(c.get(), T(0));
+  c = b.negate();
+  EXPECT_EQ(c.get(), T(6));
+  c = b.substract(a);
+  EXPECT_EQ(c.get(), T(8));
+  c = a.multiply(b);
+  EXPECT_EQ(c.get(), T(4));
+
+  // test comparisons
+  c = T(3);
+  b = c;
+  a = T(0);
+  EXPECT_TRUE(b.equal(c));
+  EXPECT_FALSE(a.equal(b));
+}
+
+TEST(ModularTest, Basic) {
+  test_modular_basic<int16_t>();
+  test_modular_basic<int32_t>();
+  test_modular_basic<int64_t>();
+  test_modular_basic<uint16_t>();
+  test_modular_basic<uint32_t>();
+  test_modular_basic<uint64_t>();
+}
+
+}  // namespace number_theory
+}  // namespace tql
