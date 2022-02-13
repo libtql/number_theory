467a69500b528293e5f21ad6f93f3d78fcf632a2
Jiashuo Liang <liangjs@pku.edu.cn>
2022-02-13 17:09:15 +0800

    Implement Euler's Sieve

---
+++ number_theory/sieve.h
@@ -0,0 +1,94 @@
+#ifndef NUMBER_THEORY_SIEVE_H_
+#define NUMBER_THEORY_SIEVE_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <bit>
+#include <limits>
+#include <stdexcept>
+#include <vector>
+
+#include "number_theory/utility.h"
+
+namespace tql {
+namespace number_theory {
+
+// Sieve of Euler.
+// It finds all prime numbers under a certain limit.
+// It also provides the factorizations of all numbers under the limit.
+//
+// The numbers are of type |T|, but they are converted to type |U| during
+// multiplication to avoid overflow.
+template <typename T, typename U = uint64_t>
+class Sieve {
+  static_assert(std::numeric_limits<T>::is_integer &&
+                    std::numeric_limits<U>::is_integer,
+                "Sieve must use integer types.");
+
+ public:
+  // The type of numbers used by the Sieve.
+  using type = T;
+
+  // Constructs the Sieve in linear time.
+  explicit Sieve(T num_limit)
+      : num_limit_(std::move(num_limit)), min_prime_factor_(num_limit_, T(0)) {
+    // Detect overflow.
+    size_t num_limit_width =
+        std::bit_width(static_cast<std::make_unsigned_t<T>>(num_limit_));
+    if (num_limit_width * 2 > std::numeric_limits<U>::digits)
+      throw std::overflow_error(
+          "Multiplication will overflow when sieving. "
+          "Please use larger integer types.");
+
+    // Euler's Sieve algorithm
+    for (T num = 2; num < num_limit_; ++num) {
+      if (min_prime_factor_[num] == 0) {
+        primes_.push_back(num);
+        min_prime_factor_[num] = num;
+      }
+      for (const T &prime : primes_) {
+        if (prime > min_prime_factor_[num])
+          break;
+        U x = static_cast<U>(prime) * static_cast<U>(num);
+        if (x >= static_cast<U>(num_limit_))
+          break;
+        min_prime_factor_[x] = prime;
+      }
+    }
+  }
+
+  Sieve(const Sieve &other) = default;
+  Sieve(Sieve &&other) = default;
+  Sieve &operator=(const Sieve &other) = default;
+  Sieve &operator=(Sieve &&other) = default;
+
+  // Returns the list of primes.
+  const std::vector<T> &primes() const { return primes_; }
+
+  // Returns the minimum prime factor of the |number|.
+  T min_prime_factor(const T &number) const {
+    auto abs_num = unsigned_abs(number);
+    if (abs_num <= 1)
+      throw std::domain_error("Minimum prime factor does not exist.");
+    if (abs_num >= num_limit_)
+      throw std::out_of_range("The number exceeds the limit of Sieve.");
+    return min_prime_factor_[abs_num];
+  }
+
+ private:
+  // The maximum number (exclusive) we can hold.
+  T num_limit_;
+  // Minimum prime factors.
+  std::vector<T> min_prime_factor_;
+  // Prime numbers.
+  std::vector<T> primes_;
+};
+
+}  // namespace number_theory
+
+using number_theory::Sieve;
+
+}  // namespace tql
+
+#endif  // NUMBER_THEORY_SIEVE_H_
---
+++ number_theory/sieve_unittest.cpp
@@ -0,0 +1,62 @@
+#include <stddef.h>
+
+#include <limits>
+#include <stdexcept>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "number_theory/sieve.h"
+
+namespace tql {
+namespace number_theory {
+
+template <typename T>
+void test_sieve_primes() {
+  Sieve sieve{T(100)};
+  std::vector<T> primes = sieve.primes();
+  std::vector<int> expected{2,  3,  5,  7,  11, 13, 17, 19, 23, 29, 31, 37, 41,
+                            43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+  ASSERT_EQ(primes.size(), expected.size());
+  for (size_t i = 0; i < primes.size(); ++i)
+    EXPECT_EQ(primes[i], expected[i]);
+}
+
+TEST(SieveTest, Primes) {
+  test_sieve_primes<int8_t>();
+  test_sieve_primes<int16_t>();
+  test_sieve_primes<int32_t>();
+  test_sieve_primes<int64_t>();
+  test_sieve_primes<uint8_t>();
+  test_sieve_primes<uint16_t>();
+  test_sieve_primes<uint32_t>();
+  test_sieve_primes<uint64_t>();
+}
+
+template <typename T>
+void test_min_prime_factor() {
+  Sieve sieve{T(100)};
+  EXPECT_EQ(sieve.min_prime_factor(15), 3);
+  EXPECT_EQ(sieve.min_prime_factor(23), 23);
+  if (std::numeric_limits<T>::is_signed) {
+    EXPECT_EQ(sieve.min_prime_factor(-15), 3);
+    EXPECT_EQ(sieve.min_prime_factor(-23), 23);
+  }
+  EXPECT_THROW(sieve.min_prime_factor(0), std::domain_error);
+  EXPECT_THROW(sieve.min_prime_factor(1), std::domain_error);
+  EXPECT_THROW(sieve.min_prime_factor(100), std::out_of_range);
+}
+
+TEST(SieveTest, MinPrimeFactor) {
+  test_min_prime_factor<int8_t>();
+  test_min_prime_factor<int16_t>();
+  test_min_prime_factor<int32_t>();
+  test_min_prime_factor<int64_t>();
+  test_min_prime_factor<uint8_t>();
+  test_min_prime_factor<uint16_t>();
+  test_min_prime_factor<uint32_t>();
+  test_min_prime_factor<uint64_t>();
+}
+
+}  // namespace number_theory
+}  // namespace tql
