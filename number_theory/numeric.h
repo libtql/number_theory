8e53efca79f9762e43dff9b8338cc2fdc9bf464e
Jiashuo Liang <liangjs@pku.edu.cn>
2022-02-18 23:30:02 +0800

    Implement integer_root

--- number_theory/numeric.h
+++ number_theory/numeric.h
@@ -1,6 +1,9 @@
 #ifndef NUMBER_THEORY_NUMERIC_H_
 #define NUMBER_THEORY_NUMERIC_H_
 
+#include <stdint.h>
+
+#include <algorithm>
 #include <cmath>
 #include <functional>
 #include <limits>
@@ -90,6 +93,55 @@ template <typename T,
           std::enable_if_t<!std::numeric_limits<U>::is_integer, bool> = true>
 auto pow(T base, U exponent) -> decltype(std::pow(base, exponent)) {
   return std::pow(base, exponent);
+}
+
+// Returns the |n|-th root of |x| rounded to integer toward zero.
+// The argument |n| shoud be positive.
+// Throws domain_error exception if the root does not exist.
+// The time complexity is O(log |x|).
+//
+// This overload is only enabled for x < 2^64, because it uses uint64_t
+// to avoid overflows during the computation.
+template <typename T,
+          typename U,
+          std::enable_if_t<std::numeric_limits<T>::digits <= 64, bool> = true>
+T integer_root(const T &x, const U &n) {
+  static_assert(
+      std::numeric_limits<T>::is_integer && std::numeric_limits<U>::is_integer,
+      "integer_root arguments must be integers.");
+  if (n < 0)
+    throw std::invalid_argument(
+        "integer_root argument |n| should be positive.");
+  if (n == 0)
+    throw std::domain_error("The 0-th root does not exist.");
+  if (x < 0 && n % 2 == 0)
+    throw std::domain_error("Negative numbers do not have even roots.");
+
+  if (n == 1)
+    return x;
+
+  // For each n, the table stores the maximum y that y^n does not overflow.
+  static const int max_size = 64;
+  static const uint64_t y_limits[max_size] = {
+      0,  0,  4294967295, 2642245, 65535, 7131, 1625, 565, 255, 138, 84, 56, 40,
+      30, 23, 19,         15,      13,    11,   10,   9,   8,   7,   6,  6,  5,
+      5,  5,  4,          4,       4,     4,    3,    3,   3,   3,   3,  3,  3,
+      3,  3,  2,          2,       2,     2,    2,    2,   2,   2,   2,  2,  2,
+      2,  2,  2,          2,       2,     2,    2,    2,   2,   2,   2,  2};
+  uint64_t y_limit = (n >= max_size ? 1 : y_limits[n]);
+
+  // Binary search for the maximum y that y^n <= abs(x).
+  uint64_t abs_x = unsigned_abs(x);
+  uint64_t y_min = 0;
+  uint64_t y_max = std::min(abs_x, y_limit) + 1;
+  while (y_max - y_min > 1) {
+    uint64_t y_mid = std::midpoint(y_min, y_max);
+    if (pow(y_mid, n) > abs_x)
+      y_max = y_mid;
+    else
+      y_min = y_mid;
+  }
+  return sign(x) * static_cast<T>(y_min);
 }
 
 }  // namespace number_theory
