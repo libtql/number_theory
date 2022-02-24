65157a9c0b4d86151563cc367a1a2fbd7e9dd6ec
liangjs <liangjs@pku.edu.cn>
2022-02-24 17:24:57 +0800

    Implement coprime_pairs

--- number_theory/prime_unittest.cpp
+++ number_theory/prime_unittest.cpp
@@ -1,6 +1,8 @@
 #include <stddef.h>
 
 #include <limits>
+#include <numeric>
+#include <set>
 #include <stdexcept>
 #include <unordered_set>
 #include <vector>
@@ -81,6 +83,34 @@ TEST(EulerSieveTest, MinPrimeFactor) {
   test_min_prime_factor<uint16_t>();
   test_min_prime_factor<uint32_t>();
   test_min_prime_factor<uint64_t>();
+}
+
+template <typename T>
+void test_coprime_pairs() {
+  T n = 100;
+  auto pairs = coprime_pairs(n);
+  for (auto [x, y] : pairs) {
+    ASSERT_GE(n, x);
+    ASSERT_GE(x, y);
+    ASSERT_GE(y, T(0));
+  }
+  std::set<std::pair<T, T>> pairs_set(pairs.begin(), pairs.end());
+  for (T x = 0; x <= n; ++x)
+    for (T y = 0; y <= x; ++y) {
+      bool is_coprime = std::gcd(x, y) == 1;
+      EXPECT_EQ(pairs_set.contains(std::pair(x, y)), is_coprime);
+    }
+}
+
+TEST(CoprimeTest, CoprimePairs) {
+  test_coprime_pairs<int8_t>();
+  test_coprime_pairs<int16_t>();
+  test_coprime_pairs<int32_t>();
+  test_coprime_pairs<int64_t>();
+  test_coprime_pairs<uint8_t>();
+  test_coprime_pairs<uint16_t>();
+  test_coprime_pairs<uint32_t>();
+  test_coprime_pairs<uint64_t>();
 }
 
 }  // namespace tql::number_theory
